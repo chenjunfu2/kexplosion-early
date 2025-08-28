6093988623460407a2507c5c5dc8102f9082843e
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-08-29 01:26:27 +0800

    删除不必要内容

--- src/client/java/chenjunfu2/kexplosion/mixin/client/ExampleClientMixin.java
+++
@@ -1,8 +0,0 @@
-package chenjunfu2.kexplosion.mixin.client;
-
-import net.minecraft.client.MinecraftClient;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(MinecraftClient.class)
-public class ExampleClientMixin {
-}
