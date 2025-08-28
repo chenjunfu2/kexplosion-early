2e5bed138c75c82017a2f185f00d1f444ed86858
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-08-29 03:03:26 +0800

    Revert "修复项目"
    This reverts commit 82e0cb10ef932535c04329eec83b1ab5ee7bd914.

---
+++ src/main/java/chenjunfu2/kexplosion/Kexplosion.java
@@ -0,0 +1,9 @@
+package chenjunfu2.kexplosion;
+import net.fabricmc.api.ModInitializer;
+
+public class Kexplosion implements ModInitializer {
+	public static final String MOD_ID = "kexplosion";
+	@Override
+	public void onInitialize()
+	{}
+}
