82e0cb10ef932535c04329eec83b1ab5ee7bd914
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2025-08-29 02:37:45 +0800

    修复项目

--- src/main/java/chenjunfu2/kexplosion/Kexplosion.java
+++
@@ -1,9 +0,0 @@
-package chenjunfu2.kexplosion;
-import net.fabricmc.api.ModInitializer;
-
-public class Kexplosion implements ModInitializer {
-	public static final String MOD_ID = "kexplosion";
-	@Override
-	public void onInitialize()
-	{}
-}
